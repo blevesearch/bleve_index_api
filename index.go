801e63e2783ac26631236b9ed3720170b144cdfb
Marty Schoch <marty.schoch@gmail.com>
2020-12-04 11:34:49 -0500

    remove dump methods from index reader
    these methods are not easily supported by all indexes
    and in practice were only implemented by the
    upsidedown index.
    
    different index implementations will likely have different
    ways of dumping their contents, which cannot fit some
    generic methods.  based on this, it seems reasonable that when
    debugging/dumping contents, you first type assert and use
    methods specific to that implementation.

--- index.go
+++ index.go
@@ -83,10 +83,6 @@ type IndexReader interface {
 	ExternalID(id IndexInternalID) (string, error)
 	InternalID(id string) (IndexInternalID, error)
 
-	DumpAll() chan interface{}
-	DumpDoc(id string) chan interface{}
-	DumpFields() chan interface{}
-
 	Close() error
 }
 
