1d97edef444b75674ac11107e993becb53f32d68
Likith B <likith.b@couchbase.com>
2024-08-09 11:53:21 +0530

    MB-61640: Fuzzy Dynamic Scoring
     - Added edit distances for terms

--- index.go
+++ index.go
@@ -194,9 +194,9 @@ type TermFieldReader interface {
 }
 
 type DictEntry struct {
-	Term     string
-	Count    uint64
-	Distance uint8
+	Term         string
+	Count        uint64
+	EditDistance uint8
 }
 
 type FieldDict interface {
