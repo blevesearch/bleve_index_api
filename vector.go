6fba9620bf7854b391a5be068baaf705a5ecb111
Likith B <likith.b@couchbase.com>
2026-04-13 12:24:47 +0530

    MB-71216: Added Support for IVFRaBitQ Indexes

--- vector.go
+++ vector.go
@@ -59,6 +59,7 @@ const (
 	IndexOptimizedForMemoryEfficient = "memory-efficient" // Flat or IVF,SQ4 indexes
 	IndexBIVFWithBackingFlat         = "bivf-flat"        // BFlat or BIVF with Flat backing index
 	IndexBIVFWithBackingSQ8          = "bivf-sq8"         // BFlat or BIVF with SQ8 backing index
+	IndexOptimizedWithRaBitQ         = "rabitq"           // Flat or IVF,RaBitQ indexes
 )
 
 const DefaultIndexOptimization = IndexOptimizedForRecall
@@ -69,6 +70,7 @@ var SupportedVectorIndexOptimizations = map[string]int{
 	IndexOptimizedForMemoryEfficient: 2,
 	IndexBIVFWithBackingFlat:         3,
 	IndexBIVFWithBackingSQ8:          4,
+	IndexOptimizedWithRaBitQ:         5,
 }
 
 // Reverse maps vector index optimizations': int -> string
@@ -78,6 +80,7 @@ var VectorIndexOptimizationsReverseLookup = map[int]string{
 	2: IndexOptimizedForMemoryEfficient,
 	3: IndexBIVFWithBackingFlat,
 	4: IndexBIVFWithBackingSQ8,
+	5: IndexOptimizedWithRaBitQ,
 }
 
 func OptimizationRequiresBinaryIndex(optimization string) bool {
